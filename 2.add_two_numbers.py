0eef13f936fad995defd7c3b9123b0befd414c2a
archang <archangdcc@gmail.com>
2016-10-26 02:03:18 -0400

    add_two_numbers

---
+++ 2.add_two_numbers.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        temp = l1.val + l2.val
+        count = int(temp / 10)
+        ret = ListNode(int(temp % 10))
+        walk = ret
+
+        l1 = l1.next
+        l2 = l2.next
+        while l1 is not None:
+            if l2 is None:
+                l2 = l1
+                break
+            temp = l1.val + l2.val + count
+
+            count = int(temp / 10)
+            walk.next = ListNode(int(temp % 10))
+            walk = walk.next
+
+            l1 = l1.next
+            l2 = l2.next
+
+        while l2 is not None:
+            temp = l2.val + count
+            count = int(temp / 10)
+            walk.next = ListNode(int(temp % 10))
+            walk = walk.next
+            l2 = l2.next
+
+        if count == 1:
+            walk.next = ListNode(1)
+
+        return ret
