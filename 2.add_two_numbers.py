86837d055bdeddac0c4caf3862f5b0521d74e88b
archang <archangdcc@gmail.com>
2016-10-26 02:19:37 -0400

    add_two_numbers: replace divide and mod

--- 2.add_two_numbers.py
+++ 2.add_two_numbers.py
@@ -15,36 +15,33 @@ class Solution(object):
         :rtype: ListNode
         """
         temp = l1.val + l2.val
-        count = int(temp / 10)
-        ret = ListNode(int(temp % 10))
+        (temp, count) = (temp, 0) if temp < 10 else (temp - 10, 1)
+        ret = ListNode(temp)
         walk = ret
 
         l1 = l1.next
         l2 = l2.next
-        while True:
-            try:
-                temp = l1.val + l2.val + count
-            except:
-                if l2 is None:
-                    l2 = l1
+        while l1 is not None:
+            if l2 is None:
+                l2 = l1
                 break
+            temp = l1.val + l2.val + count
 
-            count = int(temp / 10)
-            walk.next = ListNode(int(temp % 10))
+            (temp, count) = (temp, 0) if temp < 10 else (temp - 10, 1)
+            walk.next = ListNode(temp)
             walk = walk.next
 
             l1 = l1.next
             l2 = l2.next
 
-        # while l2 is not None:
-        while True:
-            try:
-                temp = l2.val + count
-            except:
-                if count == 1:
-                    walk.next = ListNode(1)
-                return ret
-            count = int(temp / 10)
-            walk.next = ListNode(int(temp % 10))
+        while l2 is not None:
+            temp = l2.val + count
+            (temp, count) = (temp, 0) if temp < 10 else (temp - 10, 1)
+            walk.next = ListNode(temp)
             walk = walk.next
             l2 = l2.next
+
+        if count == 1:
+            walk.next = ListNode(1)
+
+        return ret
