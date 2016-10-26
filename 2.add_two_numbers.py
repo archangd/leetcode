bdedab8cb50faf4e2068e6736591d15f36fb9408
archang <archangdcc@gmail.com>
2016-10-26 02:09:58 -0400

    add_two_numbers: exception is not faster

--- 2.add_two_numbers.py
+++ 2.add_two_numbers.py
@@ -21,11 +21,13 @@ class Solution(object):
 
         l1 = l1.next
         l2 = l2.next
-        while l1 is not None:
-            if l2 is None:
-                l2 = l1
+        while True:
+            try:
+                temp = l1.val + l2.val + count
+            except:
+                if l2 is None:
+                    l2 = l1
                 break
-            temp = l1.val + l2.val + count
 
             count = int(temp / 10)
             walk.next = ListNode(int(temp % 10))
@@ -34,14 +36,15 @@ class Solution(object):
             l1 = l1.next
             l2 = l2.next
 
-        while l2 is not None:
-            temp = l2.val + count
+        # while l2 is not None:
+        while True:
+            try:
+                temp = l2.val + count
+            except:
+                if count == 1:
+                    walk.next = ListNode(1)
+                return ret
             count = int(temp / 10)
             walk.next = ListNode(int(temp % 10))
             walk = walk.next
             l2 = l2.next
-
-        if count == 1:
-            walk.next = ListNode(1)
-
-        return ret
